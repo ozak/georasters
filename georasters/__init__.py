8acadaf4dbc6b263e2d152537c2d3697b0fe9490
Ömer Özak <ozak@users.noreply.github.com>
2015-05-11 23:22:20 -0400

    version 0.3a

--- georasters/__init__.py
+++ georasters/__init__.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 from .georasters import get_geo_info, map_pixel, map_pixel_inv, aggregate, create_geotiff, align_rasters, \
                         load_tiff, union, GeoRaster, RasterGeoError, RasterGeoTError, RasterGeoTWarning, merge, \
-                        from_file, to_pandas, to_geopandas, from_pandas, 
+                        from_file, to_pandas, to_geopandas, from_pandas 
 
 __all__ = (['get_geo_info','map_pixel', 'map_pixel_inv','aggregate','create_geotiff','align_rasters', \
             'load_tiff', 'union', 'GeoRaster', 'RasterGeoError', 'RasterGeoTError', \
