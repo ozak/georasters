f1f152af7969c5fc5ff252de7e74112c162af195
Ömer Özak <ozak@users.noreply.github.com>
2016-12-29 12:21:08 -0600

    anaconda.org dist scripts

---
+++ anaconda-dist/build-dist.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/bash
+source activate GeoPython2env
+
+# Build georasters
+conda skeleton pypi georasters
+conda build -c conda-forge -c ozak --python 2.7 --skip-existing georasters
+conda build -c conda-forge -c ozak --python 3.5 --skip-existing georasters
+conda build purge
+
+# if it fails, you need to run these
+conda skeleton pypi geopandas
+conda build -c conda-forge --python 2.7 geopandas
+conda build -c conda-forge --python 3.5 geopandas
+conda build purge
+
+conda skeleton pypi dask
+conda build -c conda-forge --python 2.7 dask
+conda build -c conda-forge --python 3.5 dask
+conda build purge
+
+# Convert to other OS
+conda convert --platform all ~/Anaconda3/conda-bld/osx-64/georasters-0.11.0-py27_0.tar.bz2 -o ~/GitHub/python-raster-stats/anaconda-dist/
+conda convert --platform all ~/Anaconda3/conda-bld/osx-64/georasters-0.11.0-py35_0.tar.bz2 -o ~/GitHub/python-raster-stats/anaconda-dist/
+
+# Upload 
+#2.7
+anaconda upload ~/GitHub/python-raster-stats/anaconda-dist/linux-32/georasters-0.11.0-py27_0.tar.bz2
+anaconda upload ~/GitHub/python-raster-stats/anaconda-dist/linux-64/georasters-0.11.0-py27_0.tar.bz2
+anaconda upload ~/GitHub/python-raster-stats/anaconda-dist/win-32/georasters-0.11.0-py27_0.tar.bz2
+anaconda upload ~/GitHub/python-raster-stats/anaconda-dist/win-64/georasters-0.11.0-py27_0.tar.bz2
+# 3.5
+anaconda upload ~/GitHub/python-raster-stats/anaconda-dist/linux-32/georasters-0.11.0-py35_0.tar.bz2
+anaconda upload ~/GitHub/python-raster-stats/anaconda-dist/linux-64/georasters-0.11.0-py35_0.tar.bz2
+anaconda upload ~/GitHub/python-raster-stats/anaconda-dist/win-32/georasters-0.11.0-py35_0.tar.bz2
+anaconda upload ~/GitHub/python-raster-stats/anaconda-dist/win-64/georasters-0.11.0-py35_0.tar.bz2
---
+++ anaconda-dist/georasters/build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+$PYTHON setup.py install 
+
+# Add more build steps here, if they are necessary.
+
+# See
+# http://docs.continuum.io/conda/build.html
+# for a list of environment variables that are set during the build process.
+# what real Python executable to use
+PYTHON=$(which python)
+
+# find the root of the virtualenv, it should be the parent of the dir this script is in
+ENV=`$PYTHON -c "import os; print(os.path.abspath(os.path.join(os.path.dirname(\"$0\"), '..')))"`
+
+# now run Python with the virtualenv set as Python's HOME
+export PYTHONHOME=$ENV
+exec $PYTHON "$@"
