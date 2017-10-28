3b9ef597930f144592415a6a770491e8bf13aedb
Ömer Özak <ozak@users.noreply.github.com>
2017-10-28 12:17:29 -0500

    Updated scripts & version

--- scripts/basic_commands.sh
+++ scripts/basic_commands.sh
@@ -2,6 +2,8 @@
 python setup.py test
 
 # To upload to PyPy
-python setup.py register
-python setup.py sdist bdist_wininst upload
+#python setup.py register
+#python setup.py sdist bdist_wheel bdist_wininst upload
+python setup.py sdist bdist_wheel --universal
+twine upload dist/*
 
