76d5949b2328ad4b83791c35a16be90b97cfdaa6
Ömer Özak <ozak@users.noreply.github.com>
2017-11-02 18:36:34 -0500

    minor edit read

--- setup.py
+++ setup.py
@@ -66,6 +66,7 @@ setup(name='georasters',
           'Programming Language :: Python :: 3',
           'Programming Language :: Python :: 3.4',
           'Programming Language :: Python :: 3.5',
+          'Programming Language :: Python :: 3.6',
       ],
       zip_safe=False,
       )
