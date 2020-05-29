3c5f33c5c039074600dd6467f7852a1d3535669f
Ömer Özak <ozak@users.noreply.github.com>
2020-05-28 19:29:13 -0500

    corrected link and added some test code

--- scripts/basic_commands.sh
+++ scripts/basic_commands.sh
@@ -9,5 +9,5 @@ python setup.py sdist bdist_wheel --universal
 twine upload dist/*
 
 # To get sha256
-wget https://files.pythonhosted.org/packages/bb/26/ba5d2443facf71d720d78c418348d05e3c6fe1c0510c556f0ff0d169f81d/georasters-0.5.16-py2.py3-none-any.whl
-openssl dgst -sha256 ./georasters-0.5.16-py2.py3-none-any.whl
+wget https://files.pythonhosted.org/packages/43/9d/bb33917c788f9283e0adf5158ab6ff070f67c971943cca1d4424645a287a/georasters-0.5.16.tar.gz
+openssl dgst -sha256 ./georasters-0.5.16.tar.gz
