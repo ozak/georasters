ac29fe7349ca21c54ba0b12e604d5d8313616943
Ömer Özak <ozak@users.noreply.github.com>
2024-02-11 20:29:26 -0600

    Update basic_commands.sh

--- scripts/basic_commands.sh
+++ scripts/basic_commands.sh
@@ -6,7 +6,7 @@ python setup.py test
 #python setup.py sdist bdist_wheel bdist_wininst upload
 python setup.py sdist
 python setup.py sdist bdist_wheel --universal
-twine upload dist/*
+twine upload --repository georasters dist/*
 
 # To get sha256
 wget https://files.pythonhosted.org/packages/62/c3/d00378a2a1f294a97c38024041a45489a8d22fbf79ed25421fdacccf784a/georasters-0.5.18.tar.gz
